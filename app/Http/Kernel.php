08ac60f9a1582bf6e449073d72ce76625858e7e2
delara 64 <delara.amiri86@yahoo.com>
2023-03-29 16:56:06 -0400

    downgrade to php 8.0.2 and all dependency

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,23 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
-    protected $middlewareAliases = [
+    // protected $middlewareAliases = [
+    //     'auth' => \App\Http\Middleware\Authenticate::class,
+    //     'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+    //     'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
+    //     'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
+    //     'can' => \Illuminate\Auth\Middleware\Authorize::class,
+    //     'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+    //     'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
+    //     'signed' => \App\Http\Middleware\ValidateSignature::class,
+    //     'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    //     'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+    // ];
+
+    protected $routeMiddleware = [
+        'student' => \App\Http\Middleware\StudentAuthenticated::class,
+        'admin' => \App\Http\Middleware\AdminAuthenticated::class,
+        'teacher' => \App\Http\Middleware\TeacherAuthenticated::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
@@ -63,11 +79,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-    ];
-
-    protected $routeMiddleware = [
-        'student' => \App\Http\Middleware\StudentAuthenticated::class,
-        'admin' => \App\Http\Middleware\AdminAuthenticated::class,
-        'teacher' => \App\Http\Middleware\TeacherAuthenticated::class,
     ];
 }
